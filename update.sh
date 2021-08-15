05b7420c40e4c064d7ed2bf5ecf46b03d144e92b
vtmx <vtmx22@gmail.com>
2021-08-15 16:14:50 -0300

    update configs

--- update.sh
+++ update.sh
@@ -15,9 +15,6 @@ cp ~/.config/fish/config.fish $dist
 echo Update nvim...
 cp ~/.config/nvim/init.vim $dist
 
-echo update rofi...
-cp ~/.config/rofi/config.rasi $dist
-
 echo update kde...
 cp ~/.config/kglobalshortcutsrc $dist
 cp ~/.config/khotkeysrc $dist
@@ -25,11 +22,5 @@ cp ~/.config/kwinrc $dist
 cp ~/.local/share/konsole/konsole.colorscheme $dist
 cp ~/.local/share/color-schemes/kde.colors $dist
 
-echo update sxhkd...
-cp ~/.config/sxhkd/sxhkdrc $dist
-
 echo update tmux...
 cp ~/.tmux.conf $dist
-
-echo Update zshrc...
-cp ~/.zshrc $dist
