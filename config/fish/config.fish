fcb06af92141656882e4bd8d5ff9ecd49d6e36ee
Vitor Melo <vtmx22@gmail.com>
2022-07-31 15:26:35 -0300

    update fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -37,17 +37,17 @@ end
 
 # emulates vim's cursor shape behavior
 # set the normal and visual mode cursors to a block
-set fish_cursor_default block
+# set fish_cursor_default block
 
 # set the insert mode cursor to a line
-set fish_cursor_insert line
+# set fish_cursor_insert line
 
 # set the replace mode cursor to an underscore
-set fish_cursor_replace_one underscore
+# set fish_cursor_replace_one underscore
 
 # the following variable can be used to configure cursor shape in
 # visual mode, but due to fish_cursor_default, is redundant here
-set fish_cursor_visual block
+# set fish_cursor_visual block
 
 # plugin ssh
 # fish_ssh_agent
