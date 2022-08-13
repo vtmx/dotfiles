5fb4af5b78ab49ee6bdf7390602a988d1f75cc3b
Vitor Melo <vtmx22@gmail.com>
2022-08-13 15:05:33 -0300

    update nvim color

--- config/nvim/lua/theme.lua
+++ config/nvim/lua/theme.lua
@@ -21,69 +21,81 @@ onedarkpro.setup {
 
 onedarkpro.load()
 
--- make bg transparent 
--- cmd('hi! Normal guibg=none ctermbg=none')
--- cmd('hi! NonText guibg=none guifg=none ctermbg=none ctermfg=none')
+function hi(group, bg, fg, gui)
+	if bg then
+		bg = 'guibg=' .. bg
+	else
+		bg = 'guibg=NONE'
+	end
 
--- bg
---cmd('hi Normal guibg=' .. c.shade0)
+	if fg then
+		fg = 'guifg=' .. fg
+	else
+		fg = 'guifg=NONE'
+	end
+
+	if gui then
+		gui = 'gui=' .. gui
+	else
+		gui = 'gui=NONE'
+	end
+
+	cmd('hi ' .. group .. ' ' .. bg .. ' ' .. fg .. ' ' .. gui)
+end
+
+-- default
+hi('Normal', c.shade0, c.shade7)
 
 -- line tilde blank
-cmd('hi EndOfBuffer guifg=' .. c.shade0)
+hi('EndOfBuffer', 'NONE', c.shade0)
 
 -- cursor line
-cmd('hi CursorLine guibg=' .. c.shade4)
+hi('CursorLine', c.shade4)
 
 -- line column
-cmd('hi LineNR guibg=' .. c.shade0)
-cmd('hi LineNR guifg=' .. c.shade3)
-cmd('hi CursorLineNr guibg=' .. c.shade0)
-cmd('hi CursorLineNr guifg=' .. c.shade7)
+hi('LineNR', c.shade0, c.shade3)
+hi('CursorLineNr', c.shade0, c.shade7)
 
 -- select text
-cmd('hi Visual guibg='..c.shade2)
+hi('Visual', c.shade2)
 
 -- menu pmenu
-cmd('hi Pmenu guibg=' .. c.shade1)
-cmd('hi PmenuSbar guibg=' .. c.shade1)
-cmd('hi PmenuSel guibg=' .. c.shade2)
-cmd('hi PmenuSel guifg=' .. c.shade7)
-cmd('hi PmenuThumb guibg=' .. c.shade2)
+hi('Pmenu', c.shade1)
+hi('PmenuSbar', c.shade1)
+hi('PmenuSel', c.shade2)
+hi('PmenuSel', 'NONE', c.shade7)
+hi('PmenuThumb', c.shade2)
 
 -- line splits
-cmd('hi VertSplit guibg=' .. c.shade0)
-cmd('hi VertSplit guifg=' .. c.shade3)
+hi('VertSplit', c.shade0, c.shade3)
 
 -- status line
-cmd('hi StatusLine guibg=' .. c.shade0)
-cmd('hi StatusLine guifg=' .. c.shade3)
-cmd('hi StatusLineNC guibg=' .. c.shade0)
-cmd('hi StatusLineNC guifg=' .. c.shade3)
+hi('StatusLine', c.shade0, c.shade3)
+hi('StatusLineNC', c.shade0, c.shade3)
 
 -- hi comments
-cmd('hi comment gui=italic guifg=' .. c.shade3)
+hi('Comment', 'NONE', c.shade3, 'italic')
 
 -- hop
-cmd('hi HopNextKey guibg=none guifg=' .. c.accent0)
-cmd('hi HopUnmatched guifg=' .. c.shade3)
+hi('HopNextKey', 'NONE', c.accent0)
+hi('HopUnmatched', 'NONE', c.shade3)
 
 -- indent-line
-cmd('hi IndentBlanklineChar guifg=' .. c.shade2)
-cmd('hi IndentBlanklineSpaceChar guibg=none')
+hi('IndentBlanklineChar', 'NONE', c.shade2)
+hi('IndentBlanklineSpaceChar')
 
 -- nvim-tree
-cmd('hi NvimTreeVertSplit guibg=' .. c.shade0)
-cmd('hi NvimTreeVertSplit guifg=' .. c.shade0)
-cmd('hi NvimTreeFolder guifg=' .. c.shade7)
-cmd('hi NvimTreeFolderIcon guifg=' .. c.shade7)
-cmd('hi NvimTreeFolderName guifg=' .. c.shade7)
-cmd('hi NvimTreeEmptyFolderName guifg=' .. c.shade7)
-cmd('hi NvimTreeOpenedFolderName guifg=' .. c.shade7)
-cmd('hi NvimTreeNormal guibg=' .. c.shade1)
-cmd('hi NvimTreeNormalNC guibg=' .. c.shade1)
-cmd('hi NvimTreeRootFolder guifg=' .. c.shade7)
-cmd('hi NvimTreeOpenedFile guifg=' .. c.shade7)
-cmd('hi NvimTreeWindowPicker guifg=' .. c.shade7)
+hi('NvimTreeVertSplit', c.shade0, c.shade0)
+hi('NvimTreeFolder', 'NONE', c.shade7)
+hi('NvimTreeFolderIcon', 'NONE', c.shade7)
+hi('NvimTreeFolderName', 'NONE', c.shade7)
+hi('NvimTreeEmptyFolderName', 'NONE', c.shade7)
+hi('NvimTreeOpenedFolderName', 'NONE', c.shade7)
+hi('NvimTreeNormal', c.shade1)
+hi('NvimTreeNormalNC', c.shade1)
+hi('NvimTreeRootFolder', 'NONE', c.shade7)
+hi('NvimTreeOpenedFile', 'NONE', c.shade7)
+hi('NvimTreeWindowPicker', 'NONE', c.shade7)
 
 -- :hi to see colors
 
