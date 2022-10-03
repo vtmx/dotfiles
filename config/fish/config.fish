4321823139e82c7ffebaa755098abfbbb87106a4
Vitor Melo <vtmx22@gmail.com>
2022-10-02 23:31:37 -0300

    update fish theme for fzf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -59,6 +59,15 @@ export CODE='code'
 export EDITOR='nvim'
 export VISUAL='nvim'
 
+# fzf
+if type -q fzf
+  set -gx FZF_DEFAULT_OPTS '
+  --color=fg:#abb2bf,bg:#23272e,hl:#61afef
+  --color=fg+:#abb2bf,bg+:#2c313c,hl+:#61afef,gutter:#23272e
+  --color=info:#5c6370,prompt:#98c379,pointer:#abb2bf
+  --color=marker:#abb2bf,spinner:#abb2bf,header:#abb2bf'
+end
+
 # alias
 # commands
 alias ...='cd ../..'
