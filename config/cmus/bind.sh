009a65259cbb4471ee03318b275102048bee433e
Vitor Melo <vtmx22@gmail.com>
2022-11-11 23:18:29 -0300

    update dotfiles

---
+++ config/cmus/bind.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# remote command
+cmus="cmus remote -C"
+
+# unbind
+$cmus unbind common ,
+$cmus unbind common =
+$cmus unbind common -
+$cmus unbind common h
+$cmus unbind common l
+$cmus unbind common m
+$cmus unbind common right
+$cmus unbind common left
+
+# up down
+$cmus bind common J win-down 5
+$cmus bind common K win-up 5
+
+# control
+$cmus bind common space player-pause
+$cmus bind common ^N player-next
+$cmus bind common ^P player-prev
+
+# seek
+$cmus bind common l seek +10
+$cmus bind common h seek -10
+$cmus bind common right seek +5
+$cmus bind common left seek -5
+
+# vol
+$cmus bind common = vol +5%
+$cmus bind common - vol -5% 
+$cmus bind common m mute
+
+# settings
+$cmus bind common , view settings
