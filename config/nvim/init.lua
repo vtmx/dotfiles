28760a94be09b2c90781e24fa1eed23ff33d2ab2
Vitor Melo <vtmx22@gmail.com>
2023-03-04 22:05:00 -0300

    update nvim and kitty

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,19 +1,6 @@
 require('config.options')
 require('config.autocmds')
+require('config.utils')
 require('config.keymaps')
-
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup('plugins')
+require('config.lazy')
 require('config.theme')
