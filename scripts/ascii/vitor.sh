20543889042885ff22988453b31a02c09d7b2a23
vtmx <vtmx22@gmail.com>
2023-12-21 08:42:54 -0300

    update ascii

--- scripts/ascii/vitor.sh
+++ scripts/ascii/vitor.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# http://www.patorjk.com/software/taag/#p=display&f=Tmplr&t=VITOR%20MELO
+# http://www.patorjk.com/software/taag/#p=display&f=Chunky&t=VITOR%20MELO
+# http://www.patorjk.com/software/taag/#p=display&f=Cyberlarge&t=VITOR%20MELO
+
+# Cyberlarge
+# figlet -c -f dotfiles/fonts/Cyberlarge/Cyberlarge.flf VITOR MELO
+
 echo " _    _ _____ _______  _____   ______ _______ _______         _____ 
   \  /    |      |    |     | |_____/ |  |  | |______ |      |     |
    \/   __|__    |    |_____| |    \_ |  |  | |______ |_____ |_____|"
