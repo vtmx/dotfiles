c2dab2c44a71d537a5270036511ee8be9d009fb5
vtmx <vtmx22@gmail.com>
2024-02-10 16:57:13 -0300

    remove bin nvim

--- config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require('config.options')
-require('config.autocmds')
-require('config.utils')
-require('config.keymaps')
-require('config.lazy')
-require('config.theme')
--- config/nvim/lua/plugins/syntaxes.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { 'khaveesh/vim-fish-syntax', ft = 'fish' },
-  { 'fladson/vim-kitty', ft = 'kitty' },
-  { 'baskerville/vim-sxhkdrc', ft = 'sxhkdrc' },
-  { 'ollykel/v-vim', ft = 'v' }
-}
