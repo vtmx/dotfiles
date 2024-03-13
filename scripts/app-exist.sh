55f6698eb81eef9da15220b3bc5fc9af1b90867b
vtmx <vtmx22@gmail.com>
2024-03-12 22:51:45 -0300

    update scripts

--- scripts/app-exist.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if type firefox2 >& /dev/null; then  
-  echo Firefox existe
-else
-  echo Firefox não existe
-fi
-
--- scripts/ide.sh
+++
@@ -1,2 +0,0 @@
-#/usr/local/bin
-tmux split-window -v -p 20
