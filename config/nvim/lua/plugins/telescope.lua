57935973283f9318270f196ca526f2cba20b865b
vtmx <vtmx22@gmail.com>
2024-10-10 15:39:11 -0300

    Update nvim telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -6,25 +6,10 @@ return {
     config = function()
       require('telescope').setup({
         defaults = {
-          disable_devicons = true,
           color_devicons = false,
           sorting_strategy = 'ascending',
           prompt_prefix = '   ',
-          layout_config = {
-            horizontal = {
-              prompt_position = 'top'
-            }
-          },
-           mappings = {
-            n = {
-              ['<c-c>'] = function(...) return require('telescope.actions').close(...) end
-            },
-            i = {
-              ['<c-j>'] = function(...) return require('telescope.actions').move_selection_next(...) end,
-              ['<c-k>'] = function(...) return require('telescope.actions').move_selection_previous(...) end,
-              ['<c-c>'] = function(...) return require('telescope.actions').close(...) end
-            }
-          }
+          layout_config = { horizontal = { prompt_position = 'top' } },
         }
       })
       require('telescope').load_extension('file_browser')
@@ -33,7 +18,6 @@ return {
   {
     'nvim-telescope/telescope-file-browser.nvim',
     cmd = 'Telescope file_browser',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-lua/plenary.nvim' }
   }
 }
 
