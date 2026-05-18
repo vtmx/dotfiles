fa095611878a70243909f26b5d1d4c202a888dae
vtmx <vtmx22@gmail.com>
2026-05-18 09:53:04 -0300

    Update nvim colors

--- config/nvim/queries/markdown/highlights.scm
+++ config/nvim/queries/markdown/highlights.scm
@@ -1,15 +1,11 @@
 ;; extends
 
-(atx_heading
-  [
-    (atx_h1_marker)
-    (atx_h2_marker)
-    (atx_h3_marker)
-    (atx_h4_marker)
-    (atx_h5_marker)
-    (atx_h6_marker)
-  ] @Operator)
-
-((fenced_code_block_delimiter) @Operator)
+; (atx_heading
+;   [ (atx_h1_marker) (atx_h2_marker) (atx_h3_marker)
+;     (atx_h4_marker) (atx_h5_marker) (atx_h6_marker)
+;   ] @markup.operator)
+;
+(((fenced_code_block_delimiter)
+  (#set! "priority" 110)) @markup.heading.1)
 
 ;; vim: ft=query
