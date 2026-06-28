440829952a0eb89996ce51c8d616c05b85254bdb
Vitor Melo <vtmx22@gmail.com>
2026-06-28 18:57:08 -0300

    Update bspwm

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 # Current dir
-current_dir=$(cd $(dirname $0) && pwd)
+cwd=$(cd $(dirname $0) && pwd)
 
 menu() {
   clear
   echo "
- _    _ _____ _______  _____   ______ _______ _______         _____ 
+ _    _ _____ _______  _____   ______ _______ _______         _____
   \  /    |      |    |     | |_____/ |  |  | |______ |      |     |
    \/   __|__    |    |_____| |    \_ |  |  | |______ |_____ |_____|"
 
   read -rp "
  b. Copy bin
  c. Clone repository
- f. Copy fonts 
+ f. Copy fonts
  l. Create simbolic links
  s. Sync
  q. Exit
@@ -43,7 +43,7 @@ copy_bin() {
   bindir=${HOME}/.local/bin
   [[ ! -d $bindir ]] && echo Create bin dir...; mkdir -p $bindir
   echo Create simolic link of bin...
-  if cp -r "$current_dir/bin/"* $bindir; then
+  if cp -r "$cwd/bin/"* $bindir; then
     pause_success "Bin copied"
   else
     pause_error "Bin not copied"
@@ -54,7 +54,7 @@ copy_fonts() {
   fontdir=${HOME}/.local/share/fonts
   [[ ! -d $fontdir ]] && echo Create font dir...; mkdir -p $fontdir
   echo Copying fonts...
-  if cp -r "$current_dir/fonts/"* $fontdir; then
+  if cp -r "$cwd/fonts/"* $fontdir; then
     pause_success "Fonts copied"
   else
     pause_error "Fonts not copied"
@@ -64,81 +64,46 @@ copy_fonts() {
 # Links need the complete path
 create_link() {
   local name=$1
-  local src=$2
-  local dist=$3
+  local src="$cwd/config"
+  local dst="$HOME/.config/"
 
   [[ $name ]] && echo "Create link for $name..."
-
-  [[ -d $dist ]] || mkdir -p $dist 
-  cp -rf $src $dist
-
-  # Create a hard symlink
-  # ln -sf $src $dist
+  ln -sfi "$src/$name" "$dst/$name"
 }
 
 create_links() {
-  local config_src="$current_dir/config"
-  local config_dist="$HOME/.config/"
-   
   # bash
-  name="bash"
-  target="$config_src/bash/."*
-  link="$HOME"
-  create_link "$name" "$target" "$link"
+  name='bash'
+  ln -sf $cwd/config/bash/bashrc $HOME/.bashrc
+  create_link "$name"
 
   # bat
-  name="bat"
-  target="$config_src/bat"
-  link="$config_dist"
-  create_link "$name" "$target" "$link"
+  name='bat'
+  create_link "$name"
+
+  # btop
+  name='btop'
+  create_link "$name"
 
   # flameshot
-  name="flameshot"
-  target="$config_src/flameshot"
-  link="$config_dist"
-  create_link "$name" "$target" "$link"
+  name='flameshot'
+  create_link "$name"
 
   # kitty
-  name="kitty"
-  target="$config_src/kitty"
-  link="$config_dist"
-  create_link "$name" "$target" "$link"
+  name='kitty'
+  create_link "$name"
 
   # mpv
-  name="mpv"
-  target="$config_src/mpv"
-  link="$config_dist"
-  create_link "$name" "$target" "$link"
+  name='mpv'
+  create_link "$name"
 
   # nvim
-  name="nvim"
-  target="$config_src/nvim"
-  link="$config_dist"
-  create_link "$name" "$target" "$link"
-
-  # starship
-  name="starship"
-  target="$config_src/starship"
-  link="$config_dist"
-  create_link "$name" "$target" "$link"
-
-  # rofi
-  name="rofi"
-  target="$config_src/rofi"
-  link="$config_dist"
-  create_link "$name" "$target" "$link"
+  name='nvim'
+  create_link "$name"
 
   # tmux
-  name="tmux"
-  target="$config_src/tmux"
-  link="$config_dist"
-  create_link "$name" "$target" "$link"
-
-  # vscode
-  # name="vscode"
-  # target="$config_src/vscode"
-  # link="$config_dist/code - oss/user"
-  # create_link "$name" "$target" "$link"
+  name='tmux'
+  create_link "$name"
 
   pause_success "Links created"
 }
@@ -146,97 +111,52 @@ create_links() {
 makesync() {
   local name="$1"
   local url="$2"
-  local dist="$3"
+  local dst="$3"
 
   echo "Sync $1..."
-  if ! wget -q $url -O "$dist"; then
+  if ! wget -q $url -O "$dst"; then
     pause_error "Fail sync $1"
   fi
 }
 
 sync() {
   local name
-  local dist
+  local dst
   local confdir="$HOME/.config/"
   local url="https://raw.githubusercontent.com/vtmx/dotfiles/main/config"
-   
-  # bash
-  name="bashrc"
-  dist="$HOME"
-  url="$url/$name/.bashrc"
-  makesync "$name" "$url" "$dist"
-  name="bash aliases"
-  url="$url/$name/.bash_aliases"
-  makesync "$name" "$url" "$dist"
-  name="bash functions"
-  url="$url/$name/.bash_functions"
-  makesync "$name" "$url" "$dist"
 
   # bspwm
   name="bspwm"
-  dist="$confdir/bspwm"
+  dst="$confdir/bspwm"
   url="$url/bspwm/bspwmrc"
-  makesync "$name" "$url" "$dist"
+  makesync "$name" "$url" "$dst"
 
   # kitty
   name="kitty"
-  dist="$confdir/kitty"
+  dst="$confdir/kitty"
   url="$url/kitty/kitty.conf"
-  makesync "$name" "$url" "$dist"
+  makesync "$name" "$url" "$dst"
 
   # nvim
   name="nvim autocmds"
-  dist="$confdir/nvim/lua/config"
+  dst="$confdir/nvim/lua/config"
   url="$url/nvim/lua/config/autocmds.lua"
-  makesync "$name" "$url" "$dist"
+  makesync "$name" "$url" "$dst"
   name="nvim colors"
   url="$url/nvim/lua/config/colors.lua"
-  makesync "$name" "$url" "$dist"
+  makesync "$name" "$url" "$dst"
   name="nvim keymaps"
   url="$url/nvim/lua/config/keymaps.lua"
-  makesync "$name" "$url" "$dist"
+  makesync "$name" "$url" "$dst"
   name="nvim options"
   url="$url/nvim/lua/config/options.lua"
-  makesync "$name" "$url" "$dist"
+  makesync "$name" "$url" "$dst"
   name="nvim theme"
   url="$url/nvim/lua/config/theme.lua"
-  makesync "$name" "$url" "$dist"
+  makesync "$name" "$url" "$dst"
   name="nvim utils"
   url="$url/nvim/lua/config/utils.lua"
-  makesync "$name" "$url" "$dist"
-
-  # starship
-  name="starship"
-  dist="$confdir"
-  url="$url/bash/starship.toml"
-  makesync "$name" "$url" "$dist"
-
-  # rofi
-  name="rofi"
-  dist="$confdir/rofi"
-  url="$url/rofi/config.rasi"
-  makesync "$name" "$url" "$dist"
-
-  # sxhkd
-  name="sxhkd"
-  dist="$confdir/sxhkd"
-  url="$url/sxhkd/sxhkdrc"
-  makesync "$name" "$url" "$dist"
-
-  # tmux
-  name="tmux"
-  dist="$confdir/tmux"
-  url="$url/tmux/tmux.conf"
-  makesync "$name" "$url" "$dist"
-
-  # vscode
-  name="vscode keybindings"
-  dist="$confdir/Code - OSS/user"
-  url="$url/vscode/keybindings.json"
-  makesync "$name" "$url" "$dist"
-  name="vscode settings"
-  url="$url/vscode/settings.json"
-  makesync "$name" "$url" "$dist"
+  makesync "$name" "$url" "$dst"
 
   pause_success "Sync maked"
 }
